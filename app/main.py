faffa49e3b332d48530e4f44f598686d8b724059
Nazreen <nxmohamad@gmail.com>
2019-12-23 17:51:44 +0000

    create pickle

--- app/main.py
+++ app/main.py
@@ -2,26 +2,45 @@ import sys
 import pdftotext
 from textblob import TextBlob
 import operator
+import pickle
 
 input_folder_path = "input/"
+pickle_store_path = "cache/store.pickle"
 if len(sys.argv) < 2:
   raise Exception('ERROR: you need to input the filename as the first argument for this script.')
 
 filename = sys.argv[1]
 filename_without_extension = filename.split('.')[0]
 
+try:
+    with open(pickle_store_path,"rb") as pickle_in:
+      store = pickle.load(pickle_in)
+except FileNotFoundError:
+    print("File not accessible")
+
 with open(input_folder_path + filename, "rb") as f:
     pdf = pdftotext.PDF(f)
-# merge everything into one string
 raw_text = "".join(pdf)
-# prepare TextBlob
+
 blob = TextBlob(raw_text)
-# access noun phrases counts
+
 np_counts = blob.np_counts
-# sort noun phrases by frequency in reverse order
 np_counts_sorted = sorted(np_counts.items(),key=operator.itemgetter(1),reverse=True)
+np_counts_filtered = np_counts_sorted[:20]
 
-filtered = np_counts_sorted[:20]
+this_text_analysis = {}
+this_text_analysis['raw_text'] = raw_text
+this_text_analysis['np_counts'] = np_counts_sorted
+this_text_analysis['np_counts_sorted'] = np_counts_sorted
+this_text_analysis['np_counts_filtered'] = np_counts_filtered
 
-for item in filtered:
-  print(item)
+try:
+  store[filename_without_extension] = this_text_analysis
+except NameError:
+  with open(pickle_store_path,"wb") as pickle_out:
+    store = {}
+    store[filename_without_extension] = this_text_analysis
+    pickle.dump(store, pickle_out)
+    pickle_out.close()
+# for item in np_counts_filtered:
+#   print(item)
